21fd3727b242b4645a7ad74640a5cf2bfe93dca4
Giorgio Giannotta <47144634+whosjorge23@users.noreply.github.com>
2020-08-07 17:38:33 +0200

    Music Player With JavaScript

---
+++ script.js
@@ -0,0 +1,91 @@
+const image = document.querySelector('img')
+const title = document.getElementById('title')
+const artist = document.getElementById('artist')
+const music = document.querySelector('audio')
+const previousButton = document.getElementById('prev')
+const playButton = document.getElementById('play')
+const nextButton = document.getElementById('next')
+
+//Music
+const songs = [
+    {
+        name: 'jacinto-1',
+        displayName: 'Electric Chill Machine',
+        artist: 'Jacinto Design'
+    },
+    {
+        name: 'jacinto-2',
+        displayName: 'Seven Nation Army (Remix)',
+        artist: 'Jacinto Design'
+    },
+    {
+        name: 'jacinto-3',
+        displayName: 'Goodnight Disco Queen',
+        artist: 'Jacinto Design'
+    },
+    {
+        name: 'metric-1',
+        displayName: 'Front Row (Remix)',
+        artist: 'Metric/Jacinto Design'
+    },
+]
+
+//Check Is Playing
+let isPlaying = false
+
+//Play
+function playSong() {
+    music.play()
+    playButton.classList.replace('fa-play', 'fa-pause')
+    playButton.setAttribute('title', 'Pause')
+    isPlaying = true
+}
+
+//Pause
+function pauseSong() {
+    music.pause()
+    playButton.classList.replace('fa-pause', 'fa-play')
+    playButton.setAttribute('title', 'Play')
+    isPlaying = false
+}
+
+//Play or Pause
+playButton.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()))
+
+//Update DOM
+function loadSong(song) {
+    title.textContent = song.displayName
+    artist.textContent = song.artist
+    music.src = `music/${song.name}.mp3`
+    image.src = `img/${song.name}.jpg`
+}
+
+//Current Song
+let songIndex = 0
+
+//Next Song
+function nextSong() {
+    songIndex++
+    if (songIndex > songs.length -1) {
+        songIndex = 0
+    }
+    loadSong(songs[songIndex])
+    playSong()
+}
+
+//Previous Song
+function prevSong() {
+    songIndex--
+    if (songIndex < 0) {
+        songIndex = songs.length -1
+    }
+    loadSong(songs[songIndex])
+    playSong()
+}
+
+// On Load - Select First Song
+loadSong(songs[songIndex])
+
+// Event Listeners
+previousButton.addEventListener('click',prevSong)
+nextButton.addEventListener('click',nextSong)
